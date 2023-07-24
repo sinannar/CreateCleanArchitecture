ecad45dfc1ec97e7b2ba74a51da1dbf85163c81c
Sinan NAR <sinan.nar@gmail.com>
2023-07-25 08:36:48 +1200

    Create clean.ps1

---
+++ clean.ps1
@@ -0,0 +1,9 @@
+
+$solutionname = $args[0]
+
+Remove-Item "$solutionname" -Recurse
+# Remove-Item "Domain" -Recurse
+# Remove-Item "Infrastructure" -Recurse
+# Remove-Item "Application" -Recurse
+# Remove-Item "Presentation" -Recurse
+
